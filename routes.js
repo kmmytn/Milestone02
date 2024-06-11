e1ddc0939a49b1e80ef20c82e5ad1380efb0e8a2
lovexias <janina_bongon@dlsu.edu.ph>
2024-06-11 20:10:13 +0800

    bcrypt implementation

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const upload = require('./uploads');
 const db = require('./database');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 // Example route to get data from the database
 router.get('/data', (req, res) => {
@@ -16,15 +17,21 @@ router.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-router.post('/signup', upload.single('pfp'), (req, res) => {
+router.post('/signup', upload.single('pfp'), async (req, res) => {
     const { full_name, email, phone_number, password } = req.body;
     const profilePicturePath = req.file? req.file.path : null; // Handle file upload
+
+    if (!password) {
+        return res.status(400).json({ error: 'Password is required.' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     // Construct the SQL query
     const sql = 'INSERT INTO users (full_name, email, phone_number, password, profile_picture) VALUES (?,?,?,?,?)';
     
     // Execute the query
-    db.query(sql, [full_name, email, phone_number, password, profilePicturePath], (err, result) => {
+    db.query(sql, [full_name, email, phone_number, hashedPassword, profilePicturePath], (err, result) => {
         if (err) {
             console.error(err);
             return res.status(500).json({ error: 'Error signing up.' });
